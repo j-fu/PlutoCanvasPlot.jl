f24120f53543eb66a90cc8e6685b130f38aab77e
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-05-14 14:14:58 +0200

    remove repository add from test notebook

--- examples/canvastest.jl
+++ examples/canvastest.jl
@@ -13,27 +13,14 @@ macro bind(def, element)
     end
 end
 
-# ╔═╡ 70437458-a4be-477e-8ca7-7556d163b6ca
-md"""
-This notebook will add the package registry [`https://github.com/j-fu/PackageNursery`](https://github.com/j-fu/PackageNursery) to your Julia environment under `~/.julia/registries/PackageNursery`. After running this notebook you can safely delete this directory.
-
-Set `proceed` to `true` if you agree to this.
-"""
-
-# ╔═╡ ef039f4d-4158-4700-a7e8-d64d429922c1
-proceed=false
-
 # ╔═╡ 21b1f4b6-2172-4292-8249-35a615db4f59
-if proceed 
+begin
     using Pkg
 	Pkg.activate(mktempdir())
-
 	# Pkg.add("Revise");using Revise
 	Pkg.add("PlutoUI")
 	using PlutoUI
-	Pkg.Registry.add(RegistrySpec(url="https://github.com/j-fu/PackageNursery"))
-	Pkg.add(name="PlutoCanvasPlot",version="0.0.2")
-	
+	Pkg.add(name="PlutoCanvasPlot",rev="0addbea")
 	#Pkg.develop("PlutoCanvasPlot")
 	using PlutoCanvasPlot
 
@@ -41,7 +28,6 @@ end
 
 # ╔═╡ 825f6068-0f02-44e4-b083-1be3eb4c764d
 let
-	proceed
 	X=0:0.1:10
 	p=CanvasPlot(;xrange=X,yrange=-1:1,resolution=(600,200))
 	polyline!(p,X,sin.(X))
@@ -51,7 +37,6 @@ end
 
 # ╔═╡ 2a5510d3-7355-4095-ab43-ad6d75bba090
 let 
-	proceed
 	p=CanvasPlot(resolution=(600,300))
 	
 	axis!(p; xtics=(0:2.5:10),ytics=(-1:0.5:1))
@@ -64,7 +49,6 @@ end
 
 # ╔═╡ 405b81de-3fd9-48f7-81a7-dc8f06dfda9c
 let 
-	proceed
 	p=CanvasPlot(resolution=(300,300),xrange=-1:1,yrange=-1:1)
 	T=0:0.01:2π
 	X=0.45.*sin.(T)
@@ -75,13 +59,11 @@ end
 
 # ╔═╡ 83c16003-8196-4f2c-a602-c298f27475fb
 begin
-	proceed
 	@bind clock_t Clock(interval=0.01)
 end
 
 # ╔═╡ fd7c959b-94d6-43ff-b535-49b0883874d5
 let 
-	proceed
 	p=CanvasPlot(resolution=(300,300),xrange=-1:1,yrange=-1:1)
 	time=0.005*clock_t
 	dt=0.01
@@ -94,8 +76,6 @@ let
 end
 
 # ╔═╡ Cell order:
-# ╟─70437458-a4be-477e-8ca7-7556d163b6ca
-# ╠═ef039f4d-4158-4700-a7e8-d64d429922c1
 # ╠═21b1f4b6-2172-4292-8249-35a615db4f59
 # ╠═825f6068-0f02-44e4-b083-1be3eb4c764d
 # ╠═2a5510d3-7355-4095-ab43-ad6d75bba090
