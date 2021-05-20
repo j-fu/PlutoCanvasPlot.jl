a9afab4860f4fa1c6be481242a2fdb7eef531d0b
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-05-20 12:26:52 +0200

    export command!

--- examples/canvastest.jl
+++ examples/canvastest.jl
@@ -20,7 +20,7 @@ begin
 	# Pkg.add("Revise");using Revise
 	Pkg.add("PlutoUI")
 	using PlutoUI
-	Pkg.add(name="PlutoCanvasPlot",version="0.0.3")
+	Pkg.add(name="PlutoCanvasPlot",version="0.0.4")
 #	Pkg.develop("PlutoCanvasPlot")
 	using PlutoCanvasPlot
 
